a24ae4ab0a09d9c7ba93bf9081cec670fc2405e7
mvfbbk <bubbz@localhost.localdomain>
2018-03-29 20:04:22 +0100

    added gzip ingestion and corrected dict counter

--- BioComII/gb_parse.py
+++ BioComII/gb_parse.py
@@ -32,63 +32,71 @@ This information can be supplied within a configuration file.
 --------------------------------------------------------------------------
 Revision History:
 =================
-A_0.1   01.03.18   Alpha   By: MVF
-A_0.2   14.03.18   Alpha   By: MVF  Comment: Corrected faulty return 
-                                             statement at end of function
+A_0.1   01.03.18    Alpha   By: MVF
+A_0.2   14.03.18    Alpha   By: MVF Comment: Corrected faulty return 
+                                            statement at end of function
+A_0.3   29.03.18    Alpha   By: MVF Comment: Introduced feature allow
+                                            function to ingest GZip files
+                                             Corrected dictionary key 
+                                            counter to ensure returned
+                                            dictionaries contained correct
+                                            keys 
 """
 import os
+import glob
 import re
+import gzip
 
-#path = os.getcwd() + "/{}"
-#file = "AB003151_1.txt"
+path = os.getcwd() + "/{}"
+file = glob.glob('*.gz') # test file "AB003151_1.txt"
 
-def GenBankParser(data):
+def GenBankParser(file):
     chr_map = {}
     accession = {}
     gene = {}
     EC_number = {}
     protein_id = {}
     product = {}
-    #protein_seq = {}
-    #DNA_seq = {}
-
-    #with open(path.format(file), 'r') as data:
-    for line in data:
-        map_matches = re.findall(r'\s+/map=\"(?P<map>\w+.\w*)\"',line)
-        for i in map_matches:
-            n = 0 # counter for the accession dictionary objects
-            if len(i) > 0:
+    
+    for f in file:
+        with gzip.open(path.format(f), 'rt') as d:
+            data = d.read()
+        #             #print(line)
+            map_matches = re.findall(r'\/map=\"(?P<map>\d{1,2}.*)\"', data) #corrected regex
+            n = 0
+            for i in map_matches:
+                n += 1 # counter for the dictionary objects
+                if len(i) > 0:
+                    chr_map[n] = i
+            accession_matches = re.findall(r'ACCESSION\s+(?P<acc>[A-Z][A-Z]?[0-9][0-9][0-9][0-9][0-9][0-9]?)', data)
+            n = 0
+            for i in accession_matches:
+                n +=1 
+                if len(i) > 0:
+                    accession[n] = i
+            gene_matches = re.findall(r'\s+/gene=\"(?P<gene>\w*)\"', data)
+            n = 0
+            for i in gene_matches:
                 n += 1
-                chr_map[n] = i
-        accession_matches = re.findall(r'ACCESSION\s+(?P<acc>[A-Z][A-Z]?[0-9][0-9][0-9][0-9][0-9][0-9]?)', line)
-        for i in accession_matches:
-            n = 0 
-            if len(i) > 0:
+                if len(i) > 0:
+                    gene[n] = i
+            EC_number_matches = re.findall(r'\s+/EC_number=\"(?P<EC_number>.*)\"', data)
+            n = 0
+            for i in EC_number_matches:
+                n += 1 
+                if len(i) > 0:
+                    EC_number[n] = i
+            protein_id_matches = re.findall(r'\s+/protein_id=\"(?P<protein_id>.*)\"', data)
+            n = 0
+            for i in protein_id_matches:
+                n += 1 
+                if len(i) > 0:
+                    protein_id[n] = i
+            product_matches = re.findall(r'\s+/product=\"(?P<product>.*)\"', data)
+            n = 0
+            for i in product_matches:
                 n += 1
-                accession[n] = i
-        gene_matches = re.findall(r'\s+/gene=\"(?P<gene>\w*)\"',line)
-        for i in gene_matches:
-            n = 0 
-            if len(i) > 0:
-                n += 1
-                gene[n] = i
-        EC_number_matches = re.findall(r'\s+/EC_number=\"(?P<EC_number>.*)\"',line)
-        for i in EC_number_matches:
-            n = 0 
-            if len(i) > 0:
-                n += 1
-                EC_number[n] = i
-        protein_id_matches = re.findall(r'\s+/protein_id=\"(?P<protein_id>.*)\"',line)
-        for i in protein_id_matches:
-            n = 0 
-            if len(i) > 0:
-                n += 1
-                protein_id[n] = i
-        product_matches = re.findall(r'\s+/product=\"(?P<product>.*)\"',line)
-        for i in product_matches:
-            n = 0 
-            if len(i) > 0:
-                n += 1
-                product[n] = i
-
+                if len(i) > 0:
+                    product[n] = i
+    
     return chr_map, accession, gene, EC_number, protein_id, product
